7137d1bf8dda39a4a1e68f60adeaabc9fce1cbc6
Alpacabla <992022824@qq.com>
2020-09-15 22:20:14 +0800

    9.15

---
+++ acwing/95 dfs easy.cpp
@@ -0,0 +1,20 @@
+/*
+link: 
+tags: 
+*/
+#include<bits/stdc++.h>
+#define to_l(a) ((a)<<1)
+#define to_r(a) ((a)<<1|1)
+#define lowbit(a) ((a)&(-a))
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int int_inf=0x3f3f3f3f;
+const ll ll_inf=0x3f3f3f3f3f3f3f3f;
+const int max_n=1e5+5;
+int book[27][27];
+int sta[]
+int main()
+{
+	return 0;
+}
