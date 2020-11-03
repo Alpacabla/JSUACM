55ab055a76366b7e7e535df298fad76f7d2ff965
Alpacabla <992022824@qq.com>
2020-11-04 02:29:39 +0800

    2020.11.3

--- acwing/298 队列优化dp.cpp
+++ acwing/298 队列优化dp.cpp
@@ -1,5 +1,5 @@
 /*
-link: 
+link: https://www.acwing.com/problem/content/300/
 tags: 
 */
 #include<bits/stdc++.h>
@@ -13,7 +13,12 @@ const int int_inf=0x3f3f3f3f;
 const ll ll_inf=0x3f3f3f3f3f3f3f3f;
 const int max_n=1.6e4+5;
 int dp[max_n][2];
-int s[105],l[105],p[105];
+struct node{
+	int s,l,p;
+	bool operator < (const node & a) const {
+		return s<a.s;
+	}
+}nodes[105];
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -22,54 +27,36 @@ int main()
 	int n,m;
 	cin>>n>>m;
 	for(int i=1;i<=m;i++){
-		cin>>l[i]>>p[i]>>s[i];
+		cin>>nodes[i].l>>nodes[i].p>>nodes[i].s;
 	}
+	sort(nodes+1,nodes+1+m);
 	for(int i=1;i<=m;i++){
-		int now=i&1,last=!now;
-		int ll=s[i]-l[i],r=s[i]+l[i];
+		int now=m&1,last=m&1;
+		int ll=nodes[i].s-nodes[i].l,r=nodes[i].s+nodes[i].l;
 		deque<int> q;
 		while(!q.empty()) q.pop_front();
 		for(int j=1;j<=n;j++){
-			if(j>ll&&j<r){
-				while(!q.empty()&&j-q.front()>=l[i]){
+			if(j>ll&&j<=nodes[i].s){
+				while(!q.empty()&&j-q.front()>=nodes[i].l){
 					q.pop_front();
-					//cout<<"no"<<" ";
 				}
-				while(!q.empty()&&dp[q.back()-1][last]+(j-q.back()+1)*p[i]<=dp[j][last]+p[i]){
+				while(!q.empty()&&dp[q.back()-1][last]+(j-q.back()+1)*nodes[i].p<dp[j-1][last]+nodes[i].p){
 					q.pop_back();
 				}
-			//if(j==0) cout<<"no"<<endl;
 				q.push_back(j);
 			}
-			if(j<s[i]||j>=r){
+			if(j<nodes[i].s||j>=r){
 				dp[j][now]=max(dp[j-1][now],dp[j][last]);
-				//cout<<j<<" "<<q.front()<<"ff";
-				//cout<<dp[j][now]<<" ";
 				continue;
-			}else{
-				while(!q.empty()&&dp[q.back()-1][last]+(j-q.back()+1)*p[i]<=dp[j][last]+p[i]){
-					q.pop_back();
-				}
-			//if(j==0) cout<<"no"<<endl;
-				q.push_back(j);
-				//if(q.empty()) dp[j][now]=max(dp[j-1][now]+p[i],dp[j][last]);
-				//else{
-					//if(q.front()==0) cout<<"no"<<endl;
-					dp[j][now]=max(max(dp[j-1][now],dp[j][last]),(j-q.front()+1)*p[i]+dp[q.front()-1][last]);
-				//}
 			}
-			//cout<<j<<" "<<q.front()<<"ff";
-			// while(!q.empty()&&dp[q.back()-1][last]+(j-q.back()+1)*p[i]<=dp[j][last]+p[i]){
-			// 	q.pop_back();
-			// }
-			// //if(j==0) cout<<"no"<<endl;
-			// q.push_back(j);
-			//cout<<dp[j][now]<<" ";
+			if(j>=nodes[i].s){
+				while(!q.empty()&&j-q.front()>=nodes[i].l){
+					q.pop_front();
+				}
+				if(q.empty()) dp[j][now]=max(dp[j-1][now],dp[j][last]);
+				else dp[j][now]=max(max(dp[j-1][now],dp[j][last]),(j-q.front()+1)*nodes[i].p+dp[q.front()-1][last]);
+			}
 		}
-		cout<<endl;
-	}
-	for(int i=1;i<=n;i++){
-		cout<<dp[i][m&1]<<endl;
 	}
 	cout<<dp[n][m&1]<<endl;
 	return 0;
