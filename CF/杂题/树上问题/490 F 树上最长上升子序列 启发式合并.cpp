89a3d8f50f4d839f7494a988c4ea649e31c0e1fb
Alpacabla <992022824@qq.com>
2021-09-16 22:13:01 +0800

    2021 9.16

---
+++ CF/杂题/树上问题/490 F 树上最长上升子序列 启发式合并.cpp
@@ -0,0 +1,148 @@
+// Problem: F. Treeland Tour
+// Contest: Codeforces - Codeforces Round #279 (Div. 2)
+// URL: https://codeforces.com/contest/490/problem/F
+// Memory Limit: 256 MB
+// Time Limit: 5000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+#include<bits/stdc++.h>
+#define to_l(a) ((a)<<1)
+#define to_r(a) ((a)<<1|1)
+#define lowbit(a) ((a)&(-a))
+using namespace std;
+#define endl '\n'
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int int_inf=0x3f3f3f3f;
+const ll ll_inf=0x3f3f3f3f3f3f3f3f;
+const int max_n=1e5+5;
+int head[max_n],to[max_n<<1],nxt[max_n<<1];
+void add(int a,int b){
+	static int tot=1;
+	to[++tot]=b;
+	nxt[tot]=head[a];
+	head[a]=tot;
+	return ;
+}
+int val[max_n];
+vector<int> f[max_n],g[max_n];
+int dp[max_n];
+void merge(vector<int> &a,vector<int> &b)
+{
+	if(a.size()<b.size()){
+		swap(a,b);
+	}
+	for(int i=0;i<b.size();i++){
+		a[i]=min(a[i],b[i]);
+	}
+	return ;
+}
+bool vis[max_n];
+int siz[max_n];
+int minsiz=int_inf;
+int cen;
+void dfs(int a,int fa)
+{
+	siz[a]=1;
+	int maxsiz=0;
+	for(int i=head[a];i;i=nxt[i]){
+		int &u=to[i];
+		if(u!=fa&&!vis[u]){
+			dfs(u,a);
+			siz[a]+=siz[u];
+			maxsiz=max(maxsiz,siz[u]);
+		}
+	}
+	maxsiz=max(maxsiz,siz[fa]);
+	if(maxsiz<minsiz){
+		minsiz=maxsiz;
+		cen=a;
+	}
+	return ;
+}
+int ans=int_inf;
+void ins(vector<int> &a,int val)
+{
+	auto k=lower_bound(a.begin(),a.end(),val);
+	if(k==a.end()) a.push_back(val);
+	else *k=val;
+	return ;
+}
+void getdp(int a,int fa)
+{
+	f[a].resize(0);
+	g[a].resize(0);
+	int res=0;
+	for(int i=head[a];i;i=nxt[i]){
+		int &u=to[i];
+		if(u!=fa&&!vis[u]){
+			getdp(u,a);
+			
+			if(f[a].size()<g[u].size()){
+				for(int i=0;i<f[a].size();i++){
+					auto k=lower_bound(g[u].begin(),g[u].end(),-f[a][i]);
+					res=max(res,int(k-g[u].begin())+i+1+((k!=g[u].begin())&&(f[a][i]<val[a]&&val[a]<-*prev(k))));
+				}
+			}else{
+				for(int i=0;i<g[u].size();i++){
+					auto k=lower_bound(f[a].begin(),f[a].end(),-g[u][i]);
+					res=max(res,int(k-f[a].begin())+i+1+((k!=f[a].begin())&&(*prev(k)<val[a]&&val[a]<-g[u][i])));
+				}
+			}
+			
+			if(f[u].size()<g[a].size()){
+				for(int i=0;i<f[u].size();i++){
+					auto k=lower_bound(g[a].begin(),g[a].end(),-f[u][i]);
+					res=max(res,int(k-g[a].begin())+i+1+((k!=g[a].begin())&&(f[u][i]<val[a]&&val[a]<-*prev(k))));
+				}
+			}else{
+				for(int i=0;i<g[a].size();i++){
+					auto k=lower_bound(f[u].begin(),f[u].end(),-g[a][i]);
+					res=max(res,int(k-f[u].begin())+i+1+((k!=f[u].begin())&&(*prev(k)<val[a]&&val[a]<-g[a][i])));
+				}
+			}
+			
+			merge(f[a],f[u]);
+			merge(g[a],g[u]);
+		}
+	}
+	ins(f[a],val[a]);
+	ins(g[a],-val[a]);
+	res=max({res,int(g[a].size()),int(f[a].size())});
+	dp[a]=res;
+	return ;
+}
+// void dfz(int a)
+// {
+	// minsiz=int_inf;
+	// dfs(a,0);
+	// for(int i=head[cen];i;i=nxt[i]){
+		// int &u=to[i];
+		// if(!vis[u]){
+			// getdp(u,cen);
+			// //
+		// }
+	// }
+	// vis[cen]=true;
+// }
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++) cin>>val[i];
+	for(int i=1;i<n;i++){
+		int a,b;
+		cin>>a>>b;
+		add(a,b);
+		add(b,a);
+	}
+	getdp(1,0);
+	int ans=0;
+	for(int i=1;i<=n;i++) ans=max(ans,dp[i]);
+	cout<<ans<<endl;
+	
+	
+	return 0;
+}
